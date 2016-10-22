cc11370ed8c95665b4639dd9cc0d59a86ff91e11
Dominique Clarke <dominiquedclarke@gmail.com>
2016-10-22 09:56:36 -0500

    added cross origin

--- src/services/scriptureService.js
+++ src/services/scriptureService.js
@@ -1,7 +1,7 @@
 angular.module('dailyOffice')
 .service('scriptureService', function($http, $sce) {
   this.getDailyScripture = () => {
-    return $http.get('https://www.esvapi.org/v2/rest/readingPlanQuery?key=IP&reading-plan=bcp')
+    return $http.get('https://crossorigin.me/http://www.esvapi.org/v2/rest/readingPlanQuery?key=IP&reading-plan=bcp')
     .then(scripture => {
       console.log(scripture.data);
       //console.log($sce.trustAsHtml(scripture.data));
